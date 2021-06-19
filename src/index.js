fc9600cb90c7bb3b96063635485a8536b7e05098
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-06-20 02:57:06 +0300

    Sets styles and updates files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import App from './components/App';
+import store from "./redux/phoneBook2/store";
+import './stylesheets/main.css';
+import './stylesheets/normalize.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+     <Provider store={store.store}>
+          <PersistGate loader={null} persistor={store.persistor}>
+               <App />
+          </PersistGate>
+    </Provider>,
+    document.querySelector('#root'));
