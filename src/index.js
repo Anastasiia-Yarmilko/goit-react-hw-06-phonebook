b84211905869e6ffe9d56a734d4f8c70639f799f
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-06-20 03:58:24 +0300

    fixes mistake

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './components/App';
-import store from "./redux/phoneBook2/store";
+import store from './redux/phoneBook2/store';
 import './stylesheets/main.css';
 import './stylesheets/normalize.css';
 
